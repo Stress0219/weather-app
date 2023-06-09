1f5844215b541d613a7fe3f7f42be8bd58237aa1
Stress0219 <joan.comas12@gmail.com>
2023-06-09 06:32:39 -0400

    geolocation_added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CircularProgress from '@mui/material/CircularProgress';
 
 interface WeatherData {
   name: string;
@@ -18,8 +19,27 @@ interface WeatherData {
 
 function App() {
   const [data, setData] = useState<WeatherData | null>(null);
-
   const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+
+          const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=ed61abb5f4541ab83d848606d66312d7`;
+      
+          axios.get(url).then((response) => {
+            setData(response.data as WeatherData);
+          });
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
+  }, []);
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=ed61abb5f4541ab83d848606d66312d7`;
 
@@ -29,8 +49,7 @@ function App() {
         setData(response.data as WeatherData);
       });
       setLocation("");
-    }
-  };
+    }}
 
   return (
     <div className="app">
@@ -44,34 +63,37 @@ function App() {
         />
       </div>
       <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>{data?.name}</p>
-          </div>
-          <div className="temp">
-            {data?.main ? <h1>{data.main.temp.toFixed()}ºF</h1> : null}
-          </div>
-          <div className="description">
-            {data?.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
-        </div>
-
-        {data?.name != undefined && (
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? (
+        {data ? (
+          <div>
+            <div className="top">
+              <div className="location">
+                <p>{data.name}</p>
+              </div>
+              <div className="temp">
+                <h1>{data.main.temp.toFixed()}ºF</h1>
+              </div>
+              <div className="description">
+                <p>{data.weather[0].main}</p>
+              </div>
+            </div>
+            <div className="bottom">
+              <div className="feels">
                 <p className="bold">{data.main.feels_like.toFixed()}ºF</p>
-              ) : null}
-              <p>Feels Like</p>
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                <p className="bold">{data.main.humidity}%</p>
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+                <p>Wind Speed</p>
+              </div>
             </div>
-            <div className="humidity">
-              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
-            </div>
-            <div className="wind">
-              {data.wind ? <p className="bold">{data.wind.speed.toFixed()} MPH</p> : null}
-              <p>Wind Speed</p>
-            </div>
+          </div>
+        ) : (
+          <div className="loading">
+          <CircularProgress />
           </div>
         )}
       </div>
